b229b1f9a416f274d50a05e5b910aac5076494f3
Yuri <ypdmarch@gmail.com>
2021-12-25 19:51:41 -0300

    funcao inicializaTabuleiro melhorada

--- src/BatalhaNavalApplication.java
+++ src/BatalhaNavalApplication.java
@@ -1,13 +1,51 @@
+import java.util.Arrays;
+import java.util.Random;
+
 import letscode.impressora.ImpressoraTabuleiro;
 
 public class BatalhaNavalApplication {
+
+    public final String tiroCerto = "*";
+    public final String tiroErrado = "-";
+    public boolean isGameOver = false;
+    public static int[][] tabuleiro = new int[10][10];
+
     public static void main(String[] args) {
         System.out.println("Jogo batalha naval esta sendo iniciado...");
 
         ImpressoraTabuleiro.imprimirMsgInicial();
 
-        int[][] tabuleiro = new int[10][10];
-        boolean isGameOver = false;
-        ImpressoraTabuleiro.imprimirTabuleiro(tabuleiro);
+        int jogadorAtual = 1;
+        int NUMERO_SUBMARINOS = 5;
+        int COORDENADAS_SUBMARINOS = 2;
+        int[][] submarinos = new int[NUMERO_SUBMARINOS][COORDENADAS_SUBMARINOS];
+
+        ImpressoraTabuleiro.inicializaTabuleiro(tabuleiro);
+
+        // TODO: funcao posicionarSubmarinos nao esta funcionando direito eu acho
+        posicionarSubmarinos(submarinos);
+        System.out.println("Submarinos posicionados com sucesso!2");
+        System.out.println(Arrays.deepToString(posicionarSubmarinos(submarinos)));
+
     }
+
+    public static int[][] posicionarSubmarinos(int[][] submarinos) {
+        Random numeroAleatorio = new Random();
+
+        for (int i = 0; i < submarinos.length; i++) {
+            submarinos[i][0] = numeroAleatorio.nextInt(10); // Pegar linha do submarino aleatoriamente
+            submarinos[i][1] = numeroAleatorio.nextInt(10); // Pegar coluna do submarino aleatoriamente
+
+            // TODO: Verificar se o for loop abaixo funciona corretamente
+            // Verificar se a posicao do submarino ja esta ocupada. Nao sei se essa parte ta
+            // funcionando direito. Nao sei verificar kkkk
+            for (int anterior = 1; anterior < submarinos.length; anterior++) {
+                if (submarinos[i][0] == submarinos[anterior][0] && submarinos[i][1] == submarinos[anterior][1]) {
+                    i--;
+                }
+            }
+        }
+        return submarinos;
+    }
+
 }
--- src/letscode/impressora/ImpressoraTabuleiro.java
+++ src/letscode/impressora/ImpressoraTabuleiro.java
@@ -9,17 +9,23 @@ public class ImpressoraTabuleiro {
     System.out.println();
   }
 
-  public static void imprimirTabuleiro(int[][] tabuleiro) {
+  public static void inicializaTabuleiro(int[][] tabuleiro) {
     System.out.println("---------------------------------------------");
     System.out.println("                 JOGADOR 1                ");
     System.out.println("---------------------------------------------");
     System.out.println("|   | A | B | C | D | E | F | G | H | I | J |");
+    System.out.println("---------------------------------------------");
     for (int i = 0; i < tabuleiro.length; i++) {
+      // Troquei o eixo do tabuleiro pra botar os numeros dentro
+      // do for loop. Consegui mais facil assim.
+
       System.out.printf("| %s |", i);
       for (int j = 0; j < tabuleiro[i].length; j++) {
-        System.out.print(" - |");
+        System.out.print("   |");
+        tabuleiro[i][j] = 0;
       }
       System.out.println();
+      System.out.println("---------------------------------------------");
     }
   }
 }
